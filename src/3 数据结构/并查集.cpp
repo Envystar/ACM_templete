8408716c78a3e4fdf94b86a6aeaf36894e3494b5
Chen Mu <3331400783@qq.com>
2024-10-06 18:40:53 +0800

    update

--- src/3 数据结构/并查集.cpp
+++ src/3 数据结构/并查集.cpp
@@ -2,8 +2,7 @@
 
 //并查集(disjoint set union)
 //https://www.luogu.com.cn/problem/P3367
-class DSU {
-public:
+struct DSU {
     DSU(int n) { //初始构造
         v.resize(n + 1);
         std::iota(v.begin(), v.end(), 0);
@@ -11,13 +10,12 @@ public:
     int find(int x) { //找根
         return (v[x] == x ? x : (v[x] = find(v[x])));
     }
-    void uniset(int x, int y) { //合并集合
+    void merge(int x, int y) { //合并集合
         v[find(x)] = find(y);
     }
     bool query(int x, int y) { //是否在同一集合
         return find(x) == find(y);
     }
-private:
     std::vector<int> v;
 };
 
@@ -31,7 +29,7 @@ int main() {
         int z, x, y;
         std::cin >> z >> x >> y;
         if(z == 1) {
-            dsu.uniset(x, y);
+            dsu.merge(x, y);
         } else if(z == 2) {
             std::cout << (dsu.query(x, y) ? 'Y' : 'N') << '\n';
         }
--- src/3 数据结构/线段树优化建图.cpp
+++ src/3 数据结构/线段树优化建图.cpp
@@ -1,17 +1,17 @@
-#include bitsstdc++.h
+#include <bits/stdc++.h>
 using i64 = long long;
 
 struct STOG {
-#define ls (id1)
-#define rs (id11)
-    STOG(int n)  n(n), in(n  2), out(n  2), v(n  7) {
+#define ls (id<<1)
+#define rs (id<<1|1)
+    STOG(int n) : n(n), in(n << 2), out(n << 2), v(n * 7) {
         int tot = n;
-        auto build = [&](auto self, int id, int l, int r) -void {
+        auto build = [&](auto self, int id, int l, int r) ->void {
             if(l == r) {
                 in[id] = out[id] = l;
                 return;
             }
-            int mid = (l + r)  2;
+            int mid = (l + r) / 2;
             self(self, ls, l, mid);
             self(self, rs, mid + 1, r); 
             in[id] = ++tot;
@@ -23,16 +23,16 @@ struct STOG {
         };
         build(build, 1, 1, n);
     }
-    void update(int x, int y, int w) { 连一条从x 到 y的边，边权为w
+    void update(int x, int y, int w) { //连一条从x 到 y的边，边权为w
         v[x].emplace_back(y, w);
     }
-    model == 0 时, 从pos 到 [x, y]连边，边权为w
-    model == 1 时, 从【x, y] 到 pos连边，边权为w
+    //model == 0 时, 从pos 到 [x, y]连边，边权为w
+    //model == 1 时, 从[x, y] 到 pos连边，边权为w
     void rangeUpdate(int pos, int x, int y,int w, int model) {
         rangeUpdate(1, 1, n, pos, x, y, w, model);
     }
     void rangeUpdate(int id, int l, int r, int pos, int x, int y, int w, auto model) {
-        if(x = l && r = y) {
+        if(x <= l && r <= y) {
             if(model == 0) {
                 update(pos, in[id], w);
             } else {
@@ -40,63 +40,63 @@ struct STOG {
             }
             return;
         }
-        int mid = (l + r)  2;
-        if(x = mid) {
+        int mid = (l + r) / 2;
+        if(x <= mid) {
             rangeUpdate(ls, l, mid, pos, x, y, w, model);
         }
-        if(y  mid) {
+        if(y > mid) {
             rangeUpdate(rs, mid + 1, r, pos, x, y, w, model);
         }
     }
 #undef ls
 #undef rs
     int n;
-    stdvectorint in, out;
-    stdvectorstdvectorstdpairint, int v;
+    std::vector<int> in, out;
+    std::vector<std::vector<std::pair<int, int>>> v;
 };
 
 int main() {
-    stdiossync_with_stdio(false);
-    stdcin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int n, q, s;
-    stdcin  n  q  s;
+    std::cin >> n >> q >> s;
     STOG tr(n);
-    for(int i = 1; i = q; ++i) {
+    for(int i = 1; i <= q; ++i) {
         int opt;
-        stdcin  opt;
+        std::cin >> opt;
         if(opt == 1) {
             int pos, x, w;
-            stdcin  pos  x  w;
+            std::cin >> pos >> x >> w;
             tr.update(pos, x, w);
         } else if(opt == 2) {
             int pos, x, y, w;
-            stdcin  pos  x  y  w;
+            std::cin >> pos >> x >> y >> w;
             tr.rangeUpdate(pos, x, y, w, 0);
         } else if(opt == 3) {
             int pos, x, y, w;
-            stdcin  pos  x  y  w;
+            std::cin >> pos >> x >> y >> w;
             tr.rangeUpdate(pos, x, y, w, 1);
         }
     }
     auto &graph = tr.v;
     int m = tr.v.size() - 1;
-    stdvectori64 dp(m + 1, LLONG_MAX);
-    stdpriority_queuestdpairi64, int, stdvectorstdpairi64, int, stdgreater pq;
+    std::vector<i64> dp(m + 1, LLONG_MAX);
+    std::priority_queue<std::pair<i64, int>, std::vector<std::pair<i64, int>>, std::greater<>> pq;
     pq.emplace(0LL, s);
     while(!pq.empty()) {
         auto [w, id] = pq.top();
         pq.pop();
-        if(w = dp[id]) continue;
+        if(w >= dp[id]) continue;
         dp[id] = w;
-        for(const auto &[nxt, dx]  graph[id]) {
+        for(const auto &[nxt, dx] : graph[id]) {
             i64 ww = w + dx;
-            if(ww  dp[nxt]) {
+            if(ww < dp[nxt]) {
                 pq.emplace(ww, nxt);
             }
         }
     }
-    for(int i = 1; i = n; ++i) {
-        stdcout  (dp[i] == LLONG_MAX  -1  dp[i])   n[i == n];
+    for(int i = 1; i <= n; ++i) {
+        std::cout << (dp[i] == LLONG_MAX ? -1 : dp[i]) << " \n"[i == n];
     }
     return 0;
 }
