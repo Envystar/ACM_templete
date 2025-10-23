503ab6be0039a06c091fd522a6ab7b0c8913b559
Chen Mu <3331400783@qq.com>
2025-10-23 15:01:33 +0800

    refactor: 重新组织代码

--- src/数学/组合数学/卢卡斯定理.cpp
+++ src/数学/组合数学/卢卡斯定理.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
 using i64 = long long;
+constexpr i64 P = 19491001;    
+constexpr i64 MAXN = 19491001;
 
-i64 qpow(i64 a, i64 b, i64 p) {
-    i64 res = 1;
-    while(b) {
-        if(b & 1) {
-            res = res * a % p;
+struct Comb {
+    Comb(int n) : _fac(n + 1, 1), _inv(n + 1, 1), _invfac(n + 1, 1) {
+        for(int i = 2; i <= n; ++i) {
+            _fac[i] = _fac[i - 1] * i % P;
+            _inv[i] = (P - P / i) * _inv[P % i] % P;
+            _invfac[i] = _inv[i] * _invfac[i - 1] % P;
         }
-        a = a * a % p;
-        b >>= 1;
     }
-    return res;
+    i64 fac(int x) { return _fac[x]; }
+    i64 invfac(int x) { return _invfac[x]; }
+    i64 inv(int x) { return _inv[x]; }
+    i64 operator()(int n, int m) {
+        if(n < m || n < 0 || m < 0) return 0;
+        return _fac[n] * _invfac[m] % P * _invfac[n - m] % P;
+    }
+    std::vector<i64> _fac, _inv, _invfac; //阶乘，i的逆元，阶乘的逆元
+} comb(MAXN);
+
+i64 lucas(i64 n, i64 m) {
+    if(m == 0) return 1;
+    return lucas(n / P, m / P) * comb(n % P, m % P) % P;
 }
 
 void solve() {
-    int n, m, p;
-    std::cin >> n >> m >> p;
-    std::vector<i64> fac(p + 1, 1);
-    for(int i = 2; i <= p; ++i) {
-        fac[i] = fac[i - 1] * i % p;
-    }
-    auto comb = [&fac, &p](i64 n, i64 m) ->i64 {
-        return fac[n] * qpow(fac[m], p - 2, p) % p * qpow(fac[n - m], p - 2, p) % p;
-    };
-    auto lucas = [&fac, &p, &comb](auto self, i64 n, i64 m) ->i64 {
-        if(m == 0) return 1;
-        return self(self, n / p, m / p) * comb(n % p, m % p) % p;
-    };
-    std::cout << lucas(lucas, n + m, m) << '\n';
+    i64 n, m;
+    std::cin >> n >> m;
+    std::cout << lucas(n + m, m) << '\n';
 }
 
 //lucas定理，求大数组合数
-//https://www.luogu.com.cn/problem/P3807
+//https://www.luogu.com.cn/problem/P5160
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
